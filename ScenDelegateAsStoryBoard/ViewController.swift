6606f989cb8caa349dc159240aa67e2a7adad895
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-01-09 21:31:44 +0530

    Initial Commit

---
+++ ScenDelegateAsStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScenDelegateAsStoryBoard
+//
+//  Created by shiga on 09/01/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
